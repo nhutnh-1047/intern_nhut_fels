50cee01c0ba950a561f06750b677e5877f1aff84
Huu Nhut <ngohuunhut16@gmail.com>
2020-09-25 16:15:04 +0700

    create migration

---
+++ database/migrations/2020_09_25_054649_create_activities_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateActivitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->string('description')->nullable();
+            $table->bigInteger('activity_type')->unsigned();
+            $table->timestamps();
+        });
+
+        Schema::table('activities', function ($table) {
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('activities');
+    }
+}
